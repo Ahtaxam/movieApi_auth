79ed5e2967c394f97dace9c98943b4a0481cb1b9
Ahtaxam <64573767+Ahtaxam@users.noreply.github.com>
2022-09-12 10:57:52 +0500

    updated file

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const express = require('express')
-const mongoose = require('mongoose');
-const route = require('./routers/movie')
-const userRoute = require('./routers/user');
-const Auth = require('./routers/auth');
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const route = require("./routers/movie");
+const userRoute = require("./routers/user");
+const Auth = require("./routers/auth");
+const app = express();
 
 const port = 3000;
-const url = 'mongodb://localhost:27017/playground';
+const url = "mongodb+srv://ahtasham:osmosm11@cluster0.xzw1puy.mongodb.net/test";
 
-mongoose.connect(url)
-.then(() =>{
-    console.log('Connected to Mongodb');
-})
-.catch(() =>{
-    console.log('Error while connecting');
-})
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Connected to Mongodb");
+  })
+  .catch(() => {
+    console.log("Error while connecting");
+  });
 
-app.use('/user' , userRoute);
-app.use('/movie' , route);
-app.use('/auth' , Auth)
+app.use("/user", userRoute);
+app.use("/movie", route);
+app.use("/auth", Auth);
 
-
-
-app.listen(port , () =>{
-    console.log(`Listening on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
